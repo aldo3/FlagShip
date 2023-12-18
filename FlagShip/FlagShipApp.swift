946d7eaa04e6c8872caa6d7cca72b5cd7d3c8cba
Al Dockett <al.dockett@gmail.com>
2023-12-18 17:49:30 -0500

    Initial Commit

---
+++ FlagShip/FlagShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagShipApp.swift
+//  FlagShip
+//
+//  Created by Al Dockett on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
